a9670ed545338d095ad67a8f2d117d0cc4d6e560
eatcpcks <yannick.schutz@gmail.com>
2010-12-01 17:08:58 +0100

    links categories

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -35,7 +35,7 @@ class StaticController < ApplicationController
       @lastPhotos = Photo.find(:all, :order=>'id DESC', :limit => 5)
     end
     if @page == 'about'
-      @links = Link.find(:all, :order=>'name ASC')
+      @links = Link.find(:all, :order=>'category ASC, name ASC')
     end
     render @page
   end
