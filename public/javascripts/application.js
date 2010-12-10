932f8c23ddee25b3903b811b5e4cd27ed485d85b
eatcpcks <yannick.schutz@gmail.com>
2010-12-11 00:11:11 +0100

    selection

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,15 @@ function add_fields(link, association, content) {
   var regexp = new RegExp("new_" + association, "g")
   $(link).parent().before(content.replace(regexp, new_id));
 }
+
+function  checkImage(o) { 
+	if(o.prev().attr('checked') == true){
+		o.prev().attr('checked', false);
+		o.removeClass('selected');
+	} else{
+		o.prev().attr('checked', true);
+		o.addClass('selected');
+	}
+        
+    
+}
